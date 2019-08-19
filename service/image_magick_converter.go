d6570871f23660e08c5a69cbad2c28101f5a247b
tdv <tdv@ngs.ru>
2019-08-19 16:54:59 +0300

    Include ImageMagick

--- service/image_magick_converter.go
+++ service/image_magick_converter.go
@@ -1,10 +1,19 @@
 package service
 
+import (
+	"github.com/quirkey/magick"
+)
+
 type imageMagickConverter struct {
 	Converter
 }
 
 func (this *imageMagickConverter) Convert(buf []byte) ([]byte, string, error) {
+	if image, err := magick.NewFromBlob(buf, ""); err != nil {
+		return err
+	} else if err = image.Resize("200x100"); err != nil {
+		return err
+	}
 	return nil, "", nil
 }
 
